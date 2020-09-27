582a3d768703096b1eec9ccc40c0b16e7c1d8905
Arsh Radhanpura <arshradhanpura28@gmail.com>
2020-09-27 14:24:56 +0530

    add routes to home and about

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h1>
+                About this App
+            </h1>
+            <p className="my-1">
+                This is Full stack MERN app
+            </p>
+            <p className="bg-dark p">
+                <strong>Version:</strong>1.0.0
+            </p>
+        </div>
+    )
+}
+export default About;
